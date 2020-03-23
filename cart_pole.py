7867ed7c4865f9f21d295ff88239a010e623ff1c
Alex Petrunin <alex@alexs-mbp.mynet>
2020-03-23 16:45:27 +0200

    changed the reward and the generation of the random vector

--- cart_pole.py
+++ cart_pole.py
@@ -2,8 +2,10 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def generate_random_weigths():
-    return np.zeros(4) * 2 - 1
+    return np.random.rand(4) * 2 - 1
+
 
 def choose_action(agent_weights, observation):
     agent_decision = np.dot(agent_weights, observation)
@@ -13,27 +15,27 @@ def choose_action(agent_weights, observation):
         action = 1
     return action
 
+
 def run_episode(env, agent_weights, episode_length):
     total_reward = 0
     observation = env.reset()
     for _ in range(episode_length):
         action = choose_action(agent_weights, observation)
         observation, reward, done, info = env.step(action)
+        total_reward += reward
         if done:
             return total_reward
-        total_reward += 1
     return total_reward
 
+
 def random_search_train(env, episode_length, num_episodes):
-    best_reward = 0
     for episode in range(num_episodes):
         agent_weights = generate_random_weigths()
         reward = run_episode(env, agent_weights, episode_length)
-        if reward > best_reward:
-            best_reward = reward
         if reward == episode_length:
             return episode
     return num_episodes
+
 
 episode_length = 200
 num_of_episodes = 10000
@@ -46,7 +48,8 @@ for _ in range(num_of_search):
     num_episodes_per_search.append(num_episodes)
 env.close()
 
-print("The average number of episodes required until the score is 200 is: " + str(np.average(num_episodes_per_search)))
+print("The average number of episodes required until the score is 200 is: ", np.average(num_episodes_per_search))
 plt.hist(num_episodes_per_search)
-plt.savefig('hist_of_num_episodes_per_search.png')
-print("The histogram of number of episodes per search was saved in the local folder with the name hist_of_num_episodes_per_search.png")
+plt.savefig('hist_of_num_episodes_per_search.png', dpi=100)
+print(
+    "The histogram of number of episodes per search was saved in the local folder with the name hist_of_num_episodes_per_search.png")
